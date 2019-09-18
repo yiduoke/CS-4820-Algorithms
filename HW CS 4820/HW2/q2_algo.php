7a8af5aff2f30364249f25123b85c699fd2bae35
Library User <yiduoke822@jhs185.org>
2019-09-17 20:00:58 -0400

    almost done with q2

--- HW CS 4820/HW2/q2_algo.php
+++ HW CS 4820/HW2/q2_algo.php
@@ -1,10 +1,12 @@
 initialize R to be the set of all availabilities;
+sort R by increasing finish time;
 initialize ordered list A to be empty;
 while (R is not yet empty):
-    get i∈R that ends the earliest;
+    get i∈R; #notice i ends the earliest out of all of R
     remove i from R;
-    get availability j∈R that overlaps with i and that ends as early as possible;
-    add i and j to the end of A;;
+    if ∃ j∈R such that i and j overlap by some time:
+        get the first j∈R; #notice j ends the earliest out of all of R
+        add i and j to the end of A;
 endwhile
-return set A as set of all partners (every two consecutive elements are partners)
+return A as set of all partners (every two consecutive elements are partners)
 
