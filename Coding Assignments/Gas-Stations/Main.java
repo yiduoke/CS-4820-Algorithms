e95e55fa1c07748d4d8878b98ac2804b1dd8d4a5
Library User <yiduoke822@jhs185.org>
2019-09-22 19:34:20 -0400

    back to arraylists sigh

--- Coding Assignments/Gas-Stations/Main.java
+++ Coding Assignments/Gas-Stations/Main.java
@@ -16,7 +16,7 @@ class Main{
 
         // initializing the data structures
         long[] Opt = new long[1]; // minimum cost up until each station
-        LinkedList<Integer>[] Solutions = new LinkedList[1]; //solution up until each station
+        ArrayList<Integer>[] Solutions = new ArrayList[1]; //solution up until each station
         int[] solution_index;
         int M = 0; // total highway length
         int m = 0; // max distance between each station
@@ -34,7 +34,7 @@ class Main{
             m = Integer.parseInt(nMm[2]);
 
             Opt = new long[M+1];
-            Solutions = new LinkedList[M+1];
+            Solutions = new ArrayList[M+1];
             solution_index = new int[M+1];
 
             for (int i = 0; i < n; i++){
@@ -46,7 +46,7 @@ class Main{
                 
                 Opt[station] = cost;
                 if (station <= m){
-                    LinkedList<Integer> solution_now = new LinkedList<Integer>();
+                    ArrayList<Integer> solution_now = new ArrayList<Integer>();
                     solution_now.add(station);
                     Solutions[station] = solution_now;
                 }
@@ -68,7 +68,7 @@ class Main{
             if (Opt[i] != 0){
                 int min_station = min_index(Opt, i-m, i-1); //TODO: write min_index(int array, int start, int end) later
                 Opt[i] = Opt[i] + Opt[min_station];
-                LinkedList<Integer> prev_solution = (LinkedList<Integer>) (Solutions[min_station]).clone();
+                ArrayList<Integer> prev_solution = (ArrayList<Integer>) (Solutions[min_station]).clone();
                 prev_solution.add(i);
                 Solutions[i] = prev_solution;
             }
@@ -76,9 +76,9 @@ class Main{
 
         int min_cost_station = min_index(Opt, M-m, M);
         System.out.println(Opt[min_cost_station]);
-        LinkedList<Integer> opt_solution = Solutions[min_cost_station];
+        ArrayList<Integer> opt_solution = Solutions[min_cost_station];
         for (int i = 0; i < opt_solution.size(); i++){
-            System.out.println(opt_solution.remove());
+            System.out.println(opt_solution.get(i));
         }
     }
 }
