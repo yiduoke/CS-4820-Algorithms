95b2a943deb0bd9baa7d09ba502e3ad86d3d9e8e
Library User <yiduoke822@jhs185.org>
2019-09-22 18:27:55 -0400

    wait this compiles im shook

---
+++ HW CS 4820/HW3/Test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.Reader;
+import java.io.Writer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+
+
+class Test{
+    public static void main(String[] args){
+        long meow = Long.MAX_VALUE;
+        System.out.println(meow);
+
+        long[] woof = {1,2,5,7,2,1};
+        Arrays.sort(woof);
+        System.out.println(woof[0]);
+    }
+}
