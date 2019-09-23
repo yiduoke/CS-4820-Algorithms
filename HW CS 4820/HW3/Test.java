fbbdeb10a2362e55dbcb4336c8deb7138d703da5
yiduoke <yiduoke822@jhs185.org>
2019-09-23 19:53:10 -0400

    q2 of HW3 started

--- HW CS 4820/HW3/Test.java
+++ HW CS 4820/HW3/Test.java
@@ -18,32 +18,13 @@ class Test{
         }
 
     public static void main(String[] args){
-        
-
-        Test o = new Test();
-        
-        Test.Station meow = o.new Station();
-        meow.location = 1;
-        meow.cost = 2;
-
-        Test.Station meow1 = o.new Station();
-        meow1.location = 3;
-        meow1.cost = 1;
-
-        Test.Station meow2 = o.new Station();
-        meow2.location = 5;
-        meow2.cost = 6;
-
-        PriorityQueue<Station> pQueue = new PriorityQueue<Station>(2); 
-        pQueue.add(meow);
-        pQueue.add(meow1);
-        pQueue.add(meow2);
-
-        int size = pQueue.size();
-
-        for (int i = 0; i < size; i++){
-            Test.Station station = pQueue.poll();
-            System.out.println("location: " + station.location + " cost: " + station.cost);
+        int diff = 0;
+        for (int i = 5; i > 0; i--){
+            for (int j = 1; j <= i; j++){
+                System.out.println("[" + j + ", " + (j + diff) + "]");
+            }
+            diff++;
+            System.out.println();
         }
     }
 }
