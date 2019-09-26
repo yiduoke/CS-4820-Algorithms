2d1fee24ab1f03b26f7910f00ca0d14ceb5c1fdd
Yiduo Ke <yiduoke822@jhs185.org>
2019-09-26 05:54:27 -0400

    hw 3 q3 part a done

---
+++ HW CS 4820/HW3/q3_a_algorithm.php
@@ -0,0 +1,7 @@
+Shortest-Path(G,s,t,k)
+    Array M[0 ... k, V]
+    Define M[0,t] = 0 and M[0,v] = ∞ for all over v ∈ V
+    For i = 1, ..., k 
+    for v ∈ V in any order 
+        node w = the node that minimizes OPT(i-1,w) + edge_weight(c,w)
+        M[i,v] = min⁡(OPT(i-1,v), ⁡(OPT(i-1,w) + edge_weight(c,w)))
