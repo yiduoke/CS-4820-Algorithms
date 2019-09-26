acf6c225015fa26879baacd8fa9c9754cb56849a
Yiduo Ke <yiduoke822@jhs185.org>
2019-09-26 13:10:14 -0400

    fixed conceptual error in q3 part 3

--- HW CS 4820/HW3/q2_algo.php
+++ HW CS 4820/HW3/q2_algo.php
@@ -26,7 +26,7 @@ Opt-Matrix-Multiplication:
                 M[starting_matrix][ending_matrix] = new_opt
                 S[starting_matrix][ending_matrix] = join(S[starting_matrix][middle_matrix],
                                                          S[middle_matrix+1][ending_matrix])
-                                                    .add(middle_matrix)
+                                                    .prepend(middle_matrix)
             endif
         endfor
         diff++
@@ -34,4 +34,4 @@ Opt-Matrix-Multiplication:
     minimum_runtime = M[1][m]
     order = S[1][m]
 
-
+    
--- HW CS 4820/HW3/q3_b_algorithm.php
+++ HW CS 4820/HW3/q3_b_algorithm.php
@@ -1,7 +1,7 @@
 Get-Negative-Cycle(G,v)
-    run Shortest-Path(G,v,v)'s algorithm
-    Define x = the first column where M[x,v] != 0
-    return M[x,v]
+    run Shortest-Path(G,v,t)'s algorithm allowing n edges
+    go through M[n,v] to find two occurences of one node
+    return the sequence between the two occurences (one end inclusive, the other side exclusive)
 
 Shortest-Path(G,s,t)
     Array M[0 ... n, V]
@@ -9,17 +9,22 @@ Shortest-Path(G,s,t)
     Define M[0,t] = 0 and M[0,v] = ∞ for all over v ∈ V
     Define S[0,t] = [[t]] and S[0,v] = [[]] for all over v ∈ V
     For i = 1, ..., n
-        For v ∈ V in any order 
-            node w = the node that minimizes M[i-1,w] + edge_weight(v,w)
+        For v ∈ V in any order
+            nodes ws = a list of nodes that equally minimizes M[i-1,w] + edge_weight(v,w),
+                , w being any element from ws
             if (M[i-1,w] + edge_weight(v,w) < M[i-1,v)]) then
                 M[i,v] = M[i-1,w] + edge_weight(v,w)
-                S[i,v] = v prepended to all of S[i-1,w]'s lists
+
+                For w ∈ ws
+                    S[i,v] = v prepended to each of S[i-1,w]'s lists
+                Endfor
             else if (M[i-1,w] + edge_weight(v,w) > M[i-1,v])
                 M[i,v] = M[i-1,v]
-                S[i,v] = S[i-1,v]
+
+                S[i,v] = S[i,v]
             else 
                 M[i,v] = M[i-1,v]
-                S[i,v] = join(S[i-1,v], v prepended to all of S[i-1,w]'s lists)
+                S[i,v] = join(S[i-1,v], v prepended to each of S[i-1,w]'s lists)
             endif
         Endfor
     Endfor 
