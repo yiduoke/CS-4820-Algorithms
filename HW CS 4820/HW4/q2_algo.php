3a54ea53000b2464ee37428b6cd607dcef057385
LIbrary User <yiduoke822@jhs185.org>
2019-09-30 16:41:05 -0400

    question2 is done but im not sure if the space complexity is log n

--- HW CS 4820/HW4/q2_algo.php
+++ HW CS 4820/HW4/q2_algo.php
@@ -1,16 +1,14 @@
 Max-Profit-Helper(start_date, end_date, dates)
-    if there is only one date:
+    if start_date == end_date:
         return (start_date, start_date)
     else:
         middle_date = (start_date + end_date) / 2
         (left_buy_date, left_sell_date) = Max-Profit(0, middle_date)
         (right_buy_date, right_sell_date) = Max-Profit(middle_date + 1, end_date)
- 
-        left_lowest = date with lowest price from the left subarray
-        right_highest = date with highest price from the right subarray
 
+        left_lowest = date with lowest price from dates[start_date … middle_date]
+        right_highest = date with highest price from dates[middle_date + 1 … middle_date]
         left_half_answer = (left_buy_date, left_sell_date)
         right_half_answer = (right_buy_date, right_sell_date)
         across_answer = (left_lowest, right_highest)
-        return pair_of_maximum_profit(left_half_answer, right_half_answer, across_answer)
-
+        return the pair of maximum profit between left_half_answer, right_half_answer, and across_answer
