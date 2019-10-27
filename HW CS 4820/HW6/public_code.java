84e91d38173739c536fae0205100f46c6630db8d
Yiduo Ke <yiduoke822@jhs185.org>
2019-10-27 17:05:37 -0400

    thought of the general algorithm ffor question 1 (coding question); just need to implement it now meow

--- HW CS 4820/HW6/public_code.java
+++ HW CS 4820/HW6/public_code.java
@@ -4,7 +4,15 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 import java.io.*;
 import java.util.*;
-
+// algorithm overview: bipartite graph but each recruiter can interview more than one candidate.
+//source -> candidates; each edge has capacity 1
+//candidate -> qualified recruiters; each edge has capacity 1
+//recruiters -> sink; each edge has capacity of the recruiter
+//if the max flow on this graph is the number of candidates, then True and return pairings
+//   note: don't run the FF from the beginning; they give us a good attempt, so our residual graph will start there
+//if the max flow is less than the number of candidates, then increase the capacity of EVERY QUALIFIED recruiter of
+//uninterviewed candidates by 1 ONLY ONCE (since many candidates can go to one recruiter and we can accidentally increase capacity by more
+//than 1) and run FF from that point every recruiter whose incoming flow increased by one is part of the answer!
 class Main
 {
 	int n; // number of candidates
