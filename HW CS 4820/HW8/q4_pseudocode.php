ed48f8ae24c29aae0b01dca9b27b799e0ff4a1a8
Yiduo Ke <yiduoke822@jhs185.org>
2019-11-21 00:29:36 -0500

    starting q4

---
+++ HW CS 4820/HW8/q4_pseudocode.php
@@ -0,0 +1,7 @@
+winStrat1(phi){
+    allTrueConfigs = list of all variable assignments
+                     such that phi is true
+    for i from 1 to length(allTrueConfigs):
+        if ()
+    
+}
