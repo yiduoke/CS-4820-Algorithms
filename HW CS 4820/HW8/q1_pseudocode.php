58b8ed06e25538ddac2b8e10d6732cb5e1b4d585
Yiduo Ke <yiduoke822@jhs185.org>
2019-11-20 22:59:29 -0500

    q1 done; pretty sure it's correct

--- HW CS 4820/HW8/q1_pseudocode.php
+++ HW CS 4820/HW8/q1_pseudocode.php
@@ -1,6 +1,9 @@
 M'(x):
-    M_m = M, but with a a new line of code that sets y
-          to 1 at the end of the main function
+    M_m = M, but there's a a new line of code in the very
+          beginning that initiates y and sets it to 0,
+          everything inside main is copied to another function f,
+          rewrite main to only have two things:
+          call f and then set y to 1
     M_m(x) //run M_m on x
 
 boolean haltChecker(string M, string x){
