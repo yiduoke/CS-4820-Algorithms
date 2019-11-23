b4af649c2f49180b02f8e35adfb25590dbd207b9
Library User <libraryuser@dhcp-gs-6490.redrover.cornell.edu>
2019-11-22 19:45:23 -0500

    q4 pseudocode -- i think it's correct now

--- HW CS 4820/HW8/q4_pseudocode.php
+++ HW CS 4820/HW8/q4_pseudocode.php
@@ -3,29 +3,42 @@ boolean winStrat1(allTrueConfigs){ // list of all variable assignments that make
                                    // [[x1 = T, x2 = F], [x1 = T, x2 = T]])
     
     length = allTrueConfigs.length
-    if (length == 0){
+    if (length < 2){ // player 2 won't make any more choices
         return true;
     }
     
-    trues = list of all A ∈ allTrueConfigs where A[1] is true;
-    falses = list of all A ∈ allTrueConfigs where A[1] is false;
+    trues = list of all A ∈ allTrueConfigs where A[0] == True;
+    falses = list of all A ∈ allTrueConfigs where A[0] == False;
     
-    if (trues[2] is only T or only F || falses[2] is only T or only F){
-        // e.g. trues = [[x1 = T, x2 = F, x3 = F], [x1 = T, x2 = F, x3 = T]]
-        // x2 = F only, meaning player 1 can't fend against player two when she makes x2 = T
+    if ((∀ x ∈ trues,  x[1] is the same) &&
+        (∀ x ∈ falses, x[1] is the same)){
+        // e.g. trues  = [[x1 = T, x2 = F, x3 = F], [x1 = T, x2 = F, x3 = T]]
+        // and  falses = [[x1 = F, x2 = T, x3 = F], [x1 = F, x2 = T, x3 = T]]
+        // then when x1 == T, then x2 = F only, and
+        // when x1 == F, then x2 = T only, meaning player 1 can't fend against
+        // player two's two available options in either case
+        
         return false;
     }
     else{
-        trueFalses = list of all A ∈ trues where A[1]==T and A[2]==F;
-        trueTrues = list of all A ∈ trues where A[1]==T and A[2]==T;
+        falseTrues = list of all x ∈ falses where x[0]==F and x[1]==T;
+        falseFalses = list of all x ∈ falses where x[0]==F and x[1]==F;
+        trueFalses = list of all x ∈ trues where x[0]==T and x[1]==F;
+        trueTrues = list of all x ∈ trues where x[0]==T and x[1]==T;
         
-        falseTrues = list of all A ∈ falses where A[1]==F and A[2]==T;
-        falseFalses = list of all A ∈ falses where A[1]==F and A[2]==F;
-
-        return winStrat1(winStrat1(trueFalses.sublist(3,end)) &&
-                         winStrat1(trueTrues.sublist(3,end)) &&
-                         winStrat1(falseTrues.sublist(3,end)) &&
-                         winStrat1(falseFalses.sublist(3,end))
-                         );
+        if (∀ x ∈ trues,  x[1] is the same){ //then player 1 can't set that x to True
+            return winStrat1(falseTrues.sublist(2,end)) &&
+                   winStrat1(falseFalses.sublist(2,end))
+        }
+        else if (∀ x ∈ falses,  x[1] is the same){ //then player 1 can't set that x to False
+            return winStrat1(trueFalses.sublist(2,end)) &&
+                   winStrat1(trueTrues.sublist(2,end))
+        }
+        else{ //then player 1 can set that x to either True or False -- for now
+            return (winStrat1(falseTrues.sublist(2,end)) &&
+                   winStrat1(falseFalses.sublist(2,end))) ||
+                   (winStrat1(trueFalses.sublist(2,end)) &&
+                    winStrat1(trueTrues.sublist(2,end)))
+        }
     }
 }
